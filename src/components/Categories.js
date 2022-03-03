6bf3fdfc1d0d5e932864b82a870f60f41fdfd093
Rasulbek <rasulbekkari008@gmail.com>
2022-03-03 23:31:43 +0600

    categories active item

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useState } from "react";
 function Categories({ items, onClick }) {
+  const [activeItem, setActiveItem] = useState(null);
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((item) => (
-          <li onClick={() => onClick(item)} key={item}>
+        <li
+          className={`${activeItem === null && "active"}`}
+          onClick={() => setActiveItem(null)}
+        >
+          Все
+        </li>
+        {items.map((item, index) => (
+          <li
+            className={`${activeItem === index && "active"}`}
+            onClick={() => setActiveItem(index)}
+            key={item}
+          >
             {item}
           </li>
         ))}
