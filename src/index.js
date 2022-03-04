07681daee9e6094bd900dde0de0859b1cb043bbc
Rasulbek <rasulbekkari008@gmail.com>
2022-03-04 18:24:13 +0600

    pages and popup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import "./scss/app.scss";
 
 import App from "./App";
+import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
