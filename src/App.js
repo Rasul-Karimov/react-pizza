a1cc4db4fd654a1b2c606b756a05fce9ab475025
Rasulbek <rasulbekkari008@gmail.com>
2022-03-05 23:50:38 +0600

    pizza  block component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 function App() {
   const [pizzas, setPizzas] = useState([]);
   useEffect(function () {
-    fetch("http://localhost:3000/db.json")
-      .then((resp) => resp.json())
-      .then((json) => {
-        setPizzas(json.pizzas);
-      });
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
   }, []);
-
-  console.log(pizzas);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </div>
